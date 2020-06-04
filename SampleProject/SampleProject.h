621466d6491cb7097174d8aae7b9cc2b76efa421
joprithiviraj <prithiviraj1522@gmail.com>
2020-06-04 18:21:23 +0530

    Initial Project Setup

---
+++ SampleProject/SampleProject.h
@@ -0,0 +1,19 @@
+//
+//  SampleProject.h
+//  SampleProject
+//
+//  Created by Prithiviraj Tamilarasan on 04/06/20.
+//  Copyright © 2020 Prithiviraj Tamilarasan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleProject.
+FOUNDATION_EXPORT double SampleProjectVersionNumber;
+
+//! Project version string for SampleProject.
+FOUNDATION_EXPORT const unsigned char SampleProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleProject/PublicHeader.h>
+
+
